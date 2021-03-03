1f46923f36365e37157033cb80a0bd7d778246be
Alexander <wyndace@hotmail.com>
2021-03-03 21:27:47 +0500

    add user picker
    Добавил возможность выбирать конкретных(-ого) пользователей(-я) и чуток переписал структуру кода

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from os import listdir, chdir, path
 
 group_id = 5
 session_cookie = '<здесь кук "session" из залогиненой сессии на shadowservants.ru (для доступа к стоимости тасков)>'
+categories = 'Crypto', 'Web', 'Networking', 'PPC', 'Forensic', 'PWN', 'Reverse', 'Stegano'
+domain = 'http://shadowservants.ru'
+session = Session()
+task_cache = {}
+
 
 
 def print_rate(num):
@@ -62,37 +67,55 @@ def show_player(url, name):
         print_rate(categories_dict[category])
     print('\n', ('-'*(10 + 1) + '|')*10, sep='')
 
+def init_shower():
+    print('\n Wait...\n')
+    print(' Name      | Score     ', *['| ' + category + ' '*(10 - len(category)) for category in categories], '|', sep='')
+    print(('-'*(10 + 1) + '|')*10, sep='')
+    
+    session.cookies['session'] = session_cookie
+    html = session.get('{}/score?group_id={}'.format(domain, group_id)).text
+    soup = BeautifulSoup(html, features="html5lib")
+    table = soup.find('table').tbody
+    return table
 
-categories = 'Crypto', 'Web', 'Networking', 'PPC', 'Forensic', 'PWN', 'Reverse', 'Stegano'
-print('Подождите...')
-print(' Name      | Score     ', *['| ' + category + ' '*(10 - len(category)) for category in categories], '|', sep='')
-print(('-'*(10 + 1) + '|')*10, sep='')
-
-domain = 'http://shadowservants.ru'
-session = Session()
-session.cookies['session'] = session_cookie
-html = session.get('{}/score?group_id={}'.format(domain, group_id)).text
-soup = BeautifulSoup(html, features="html5lib")
-table = soup.find('table').tbody
-
-# load tasks from file
-chdir(path.dirname(__file__))
-if 'task_cache.json' in listdir():
-    cache_file = open('task_cache.json', 'r')
-    try:
-        task_cache = load(cache_file)
-    except decoder.JSONDecodeError:
+def task_loader():
+    # load tasks from file
+    chdir(path.dirname(__file__))
+    if 'task_cache.json' in listdir():
+        cache_file = open('task_cache.json', 'r')
+        try:
+            task_cache = load(cache_file)
+        except decoder.JSONDecodeError:
+            task_cache = {}
+    else:
         task_cache = {}
-else:
-    task_cache = {}
 
-for tr in table:
-    a = tr.find('a')
-    if type(a) == int: continue
-    name = a.text
-    show_player(a.get('href'), name)
+def show_scores():
+    task_loader()
+    for tr in init_shower():
+        a = tr.find('a')
+        if type(a) == int: continue
+        name = a.text
+        show_player(a.get('href'), name)
 
-# save tasks to file
-cache_file = open('task_cache.json', 'w')
-dump(task_cache, cache_file)
-cache_file.close()
+def show_user_score():
+    nicks = input('\n Who do you need?:\n ').split()
+    task_loader()
+    for tr in init_shower():
+        a = tr.find('a')
+        if type(a) == int: continue
+        name = a.text
+        for nick in nicks:
+            if ' ' + nick + ' ' == name:
+                show_player(a.get('href'), name)
+
+if __name__ == '__main__':
+    i = input(' Do you want to see the entire table or just a specific player? [Y/n]: ')
+    if i == "y" or i == "Y" or i == 'yes' or i == "YES" or i == "Yes" or  i =='YEs' or i == 'yeS' or i == 'yEs' or i == 'yES' or i == 'Д' or i == 'д' or i== 'Да' or i == 'ДА' or i == 'дА':
+        show_scores()
+    else:
+        show_user_score()
+    # save tasks to file
+    cache_file = open('task_cache.json', 'w')
+    dump(task_cache, cache_file)
+    cache_file.close()    
